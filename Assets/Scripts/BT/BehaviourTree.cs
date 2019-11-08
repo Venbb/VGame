79d8aab14ce3b09b37997d6800bb6efb802fbab7
biao.wen <biao.wen@pitayagames.com>
2019-11-08 18:34:45 +0800

    行为树

---
+++ Assets/Scripts/BT/BehaviourTree.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BTState
+{
+    Failure,
+    Success,
+    Running
+}
+public abstract class BTNode
+{
+    public BTState state { get; protected set; }
+    public abstract BTState Handle();
+}
---
+++ Assets/Scripts/BT/TestBT.cs
@@ -0,0 +1,63 @@
+﻿using BTAI;
+using UnityEngine;
+ 
+public class TestBT : MonoBehaviour, BTAI.IBTDebugable
+{
+    Root aiRoot = BT.Root();
+ 
+ 
+    private void OnEnable()
+    {
+        aiRoot.OpenBranch(
+                BT.If(TestVisibleTarget).OpenBranch(
+                BT.Call(Aim),
+                BT.Call(Shoot)
+                 ),
+                BT.Sequence().OpenBranch(
+                BT.Call(Walk),
+                BT.Wait(5.0f),
+                BT.Call(Turn),
+                BT.Wait(1.0f),
+                BT.Call(Turn)
+             )
+        );
+    }
+ 
+    private void Turn()
+    {
+        Debug.Log("执行了 Turn");
+    }
+ 
+    private void Walk()
+    {
+        Debug.Log("执行了 Walk");
+    }
+ 
+    private void Shoot()
+    {
+        Debug.Log("执行了 Shoot");
+    }
+ 
+    private void Aim()
+    {
+        Debug.Log("执行了 Aim");
+    }
+ 
+    private bool TestVisibleTarget()
+    {
+        var isSuccess = UnityEngine.Random.Range(0, 2) == 1;
+        Debug.Log("执行了 TestVisibleTarget    Result:" + isSuccess);
+ 
+        return isSuccess;
+    }
+ 
+    private void Update()
+    {
+        aiRoot.Tick();
+    }
+ 
+    public Root GetAIRoot()
+    {
+        return aiRoot;
+    }
+}
